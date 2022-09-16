4a734c4fffb8b49ace2a03a3f7bdc8716991ac17
Amira20208045 <amira20208045@fci.helwan.edu.eg>
2022-09-16 15:05:59 +0200

    binary search-problems

--- 349. Intersection of Two Arrays.cpp
+++ 349. Intersection of Two Arrays.cpp
@@ -48,3 +48,35 @@ public:
         return intersection;
     }
 };
+
+// solution binary search
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0 , right = nums.size()-1;
+        while(left <= right){
+          int mid = left +(right - left)/2;
+          if (nums[mid]==target) return mid;
+          else if (nums[mid]>target) right = mid - 1;
+          else left = mid + 1; 
+       }
+       return -1;     
+    }
+    
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+    
+        vector<int>ans;
+        
+        for(int i=0;i<nums1.size();i++){
+            int temp = search(nums2,nums1[i]);
+            if(temp!=-1) ans.push_back(nums2[temp]);
+        }
+        
+        sort(ans.begin(),ans.end());
+        ans.erase(unique(ans.begin(),ans.end()),ans.end());
+        
+        return ans;
+    }
+};
